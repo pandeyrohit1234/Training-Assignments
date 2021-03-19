9c51a42b2aee0bc855bada8177476088db33c78f
Rohit kumar pandey <80757398+pandeyrohit1234@users.noreply.github.com>
2021-03-19 04:20:01 -0700

    Day-5 (C#)

---
+++ C# -Day5/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace myfirstappp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter Your name");
+            var name =Console.ReadLine();
+            var date=DateTime.Now;
+            Console.WriteLine($"Hello {name} on {date}");
+            
+        }
+    }
+}
