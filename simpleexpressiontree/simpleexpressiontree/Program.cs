99d60f33893295fd3c9f87f66558ebbd1dae4b60
Rohit kumar pandey <80757398+pandeyrohit1234@users.noreply.github.com>
2021-03-26 01:51:50 -0700

    simpleexpressiontree

---
+++ simpleexpressiontree/simpleexpressiontree/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace simpleexpressiontree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Func<int, int> square = (x) => x * x;
+            System.Linq.Expressions.Expression<Func<int, int>> e = x => x * x;
+            var a = e.Compile();
+            Console.WriteLine(a(7));
+
+        }
+    }
+}
